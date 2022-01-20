269b81565bdf2d1bf4cfb6f1acc08407d32f4ef0
shadow-moon-007 <shadowsrebel@gmail.com>
2022-01-20 22:37:23 +0530

    recursion

---
+++ day7/subsequences.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printF(int ind, vector<int> &ds, int arr[], int n)
+{
+    if (ind == n)
+    {
+        for (auto it : ds)
+        {
+            cout << it << " ";
+        }
+        cout << endl;
+        return;
+    }
+    // pick
+    ds.push_back(arr[ind]);
+    printF(ind + 1, ds, arr, n);
+    ds.pop_back();
+
+    // not pick
+    printF(ind + 1, ds, arr, n);
+}
+
+int main()
+{
+    int arr[] = {3, 1, 2};
+    int n = 3;
+    vector<int> ds;
+    printF(0, ds, arr, n);
+}
