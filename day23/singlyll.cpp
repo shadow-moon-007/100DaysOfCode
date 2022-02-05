9686c67147706986322c828f330ec5d1b87740e7
shadow-moon-007 <shadowsrebel@gmail.com>
2022-02-06 00:02:19 +0530

    linked list

---
+++ day23/singlyll.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+void InsertAtTail(node *&head, int d)
+{
+    node *n = new node(d);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void InsertAtHead(node *&head, int d)
+{
+    node *n = new node(d);
+    n->next = head;
+    head = n;
+}
+
+bool search(node *&head, int key)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == key)
+        {
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
+void Display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main()
+{
+    node *head = NULL;
+    InsertAtTail(head, 1);
+    InsertAtTail(head, 2);
+    InsertAtTail(head, 3);
+    InsertAtTail(head, 4);
+    InsertAtHead(head, 5);
+    Display(head);
+    cout << search(head, 4);
+}
