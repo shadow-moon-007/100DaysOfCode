00a5ffdb146699cce225a7834f56cee81b821c1d
shadow-moon-007 <shadowsrebel@gmail.com>
2022-02-06 21:41:32 +0530

    link lest

---
+++ day24/deletionLL.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+void InsertAtTail(node *&head, int d)
+{
+    node *n = new node(d);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void InsertAtHead(node *&head, int d)
+{
+    node *n = new node(d);
+    n->next = head;
+    head = n;
+}
+
+bool search(node *&head, int key)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == key)
+        {
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
+void Display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+void deleteAtHead(node *&head)
+{
+    node *todelete = head;
+    head = head->next;
+    delete todelete;
+}
+void deletion(node *&head, int val)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+    if (head->next == NULL)
+    {
+        deleteAtHead(head);
+    }
+    node *temp = head;
+    while (temp->next->data != val)
+    {
+        temp = temp->next;
+    }
+    node *toDelete = temp->next;
+    temp->next = temp->next->next;
+
+    delete toDelete;
+}
+
+int main()
+{
+    node *head = NULL;
+    InsertAtTail(head, 1);
+    InsertAtTail(head, 2);
+    InsertAtTail(head, 3);
+    InsertAtTail(head, 4);
+    InsertAtHead(head, 5);
+    Display(head);
+    cout << search(head, 4) << endl;
+    deletion(head, 3);
+    deleteAtHead(head);
+    Display(head);
+}
